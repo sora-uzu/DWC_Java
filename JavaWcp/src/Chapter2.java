beb93d551e3fbc344a773cdeff5b22b29a5405a5
sora-uzu <naruto07170717@gmail.com>
2020-06-13 17:14:24 +0900

    [Update]Chapter4

--- JavaWcp/src/Chapter2.java
+++ JavaWcp/src/Chapter2.java
@@ -1,6 +1,13 @@
 
+//public class Chapter2 {
+//	public static void main(String[] args) {
+//        System.out.println("こんにちは!");
+//    }
+//}
+
 public class Chapter2 {
 	public static void main(String[] args) {
-        System.out.println("こんにちは!");
+        System.out.println("Hello WEBCAMP");
+        System.out.println("Hello Java");
     }
 }
