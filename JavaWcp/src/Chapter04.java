c2214226dba2f31e46f6fbc99960b91e40c9b3ce
sora-uzu <naruto07170717@gmail.com>
2020-06-17 12:03:17 +0900

    [Start]Chapter7

--- JavaWcp/src/Chapter04.java
+++ JavaWcp/src/Chapter04.java
@@ -1,4 +1,9 @@
 
 public class Chapter04 {
-
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "照井宙"
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
 }
