2f1e22ab957e830abf0b26e52ca5a7a55e489999
sora-uzu <naruto07170717@gmail.com>
2020-06-21 19:15:33 +0900

    [Update]Chapter7

--- JavaWcp/src/Swich01.java
+++ JavaWcp/src/Swich01.java
@@ -1,16 +1,29 @@
 
-public class Swich01 {
-	public static void main(String[] args) {
-		int val =2;
-		switch (val) {
-		case 3:
-			System.out.println("よくできました");
-			break;
-		case 2:
-			System.out.println("普通ですね");
-			break;
-		default:
-			System.out.println("ファイト");
-		}
-	}
+//public class Swich01 {
+//	public static void main(String[] args) {
+//		int val =2;
+//		switch (val) {
+//		case 3:
+//			System.out.println("よくできました");
+//			break;
+//		case 2:
+//			System.out.println("普通ですね");
+//			break;
+//		default:
+//			System.out.println("ファイト");
+//		}
+//	}
+//}
+
+public class Switch02 {
+    public static void main(String[] args) {
+        int val = 2;
+        if (val == 3) {
+            System.out.println("よくできました！");
+        } else if (val == 2) {
+            System.out.println("ふつうでした！");
+        } else {
+            System.out.println("がんばろう！");
+        }
+    }
 }
